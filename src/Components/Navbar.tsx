75ab2ec3340b3a21bcff4b7e67085e6c9f6c9998
Sourabh <ranakotisrk@gmail.com>
2024-09-23 06:42:40 +0530

    navbar design completed

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -8,25 +8,26 @@ export default function Navbar() {
     const [showSideBar,setShowSideBar] = useState(false);
     const [isScrolled, setIsScrolled] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 50) {
-        setIsScrolled(true);
-      } else {
-        setIsScrolled(false);
-      }
-    };
+    useEffect(() => {
+      const handleScroll = () => {
+        const offset = window.scrollY;
+        if (offset > 50) { // Adjust the scroll position as needed
+          setIsScrolled(true);
+        } else {
+          setIsScrolled(false);
+        }
+      };
+  
+      window.addEventListener('scroll', handleScroll);
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, []);
 
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
+    const IconStyle = {fontSize: '40px', color: '#FFFFFF', p: '30px'};
   return (
-    <nav className={isScrolled ? "navBarScrolled" : ""}>
-        <ul>
+    <nav>
+        <ul className={isScrolled ? 'stickyNav' : ''}>
             <li><a href="#"><Image src="/Logo.png" alt="logo" width="160" height="110"/></a></li>
             <li className="hideOnMobile"><a href="#">Home</a></li>
             <li className="hideOnMobile"><a href="#">About Us</a></li>
@@ -34,10 +35,10 @@ export default function Navbar() {
             <li className="hideOnMobile"><a href="#">Service</a></li>
             <li className="hideOnMobile"><a href="#">Contact</a></li>
             <li className="hideOnMobile"><a href="#">Job</a></li>
-            <li className="hideOnDesktop" onClick={()=>setShowSideBar(true)}><a href="#"><MenuIcon sx={{fontSize: '40px'}}/></a></li>
+            <li className="hideOnDesktop" onClick={()=>setShowSideBar(true)}><MenuIcon sx={IconStyle}/></li>
         </ul>
         <ul className={showSideBar ? 'sideBar' : 'hideSideBar'}>
-            <li onClick={()=>setShowSideBar(false)}><a href="#"><CloseIcon sx={{fontSize: '40px'}}/></a></li>
+            <li onClick={()=>setShowSideBar(false)}><CloseIcon sx={IconStyle}/></li>
             <li><a href="#">Home</a></li>
             <li><a href="#">About Us</a></li>
             <li><a href="#">Our Menu</a></li>
