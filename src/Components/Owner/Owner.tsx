5ace84a23dfb5a74717f441a1e5f16ec82e5bfb6
Sourabh <ranakotisrk@gmail.com>
2025-02-14 06:01:56 +0530

    added action on read more btn

--- src/Components/Owner/Owner.tsx
+++ src/Components/Owner/Owner.tsx
@@ -1,3 +1,4 @@
+'use client';
 import Image from "next/image";
 import OwnerStyles from "./Owner.module.css";
 
@@ -9,7 +10,7 @@ export default function Owner() {
                 <p>“We believe in creating an unmatched experience by maintaining the highest standard of quality, hygiene, service and customer satisfaction.”</p>
                 <p className={OwnerStyles.ownerName}><strong>Mr. Virendra Dutt Ranakoti,</strong></p>
                 <p className={OwnerStyles.ownerDesignation}><strong>MahaMantri, VyaparSabha Badrinath</strong></p>
-                <button className={OwnerStyles.readMoreBtn}>Read more</button>
+                <button className={OwnerStyles.readMoreBtn} onClick={() => window.location.href = '/about'}>Read more</button>
             </div>
             <div className={OwnerStyles.imageDiv}>
                 <Image src="/owner1.jpg" alt="owner" width={400} height={520}/>
