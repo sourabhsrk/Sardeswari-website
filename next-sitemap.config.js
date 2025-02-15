9fdb85d279c15026506d105f759cb232a9ea1dea
Sourabh <ranakotisrk@gmail.com>
2025-02-16 03:31:44 +0530

    added sitemap for indexing

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.sardeswari.in/', // Replace with your actual domain
+    generateRobotsTxt: true, // Generates a robots.txt file
+    priority: 1
+  };
